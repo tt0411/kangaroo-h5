56fe7cf46d7c16de6a02ab3fc8b8304bd464dc60
mengtao wang <2726653264@qq.com>
2020-01-19 13:45:23 +0800

    http -> https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 // const { NODE_ENV } = process.env
 
-export const URL = 'http://175.24.36.114:3002'
-export const MURL = 'http://175.24.36.114:3002'
+export const URL = 'https://175.24.36.114:3002'
+export const MURL = 'https://175.24.36.114:3002'
 // export const URL = 'http://localhost:3002'
 // export const MURL = 'http://localhost:3002'
